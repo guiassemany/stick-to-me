35b878b8c285591bd6cdfdecdd77c851754ecbe9
Guilherme Assemany <guilhermeassemany@hotmail.com>
2017-03-18 21:56:53 -0300

    Preparing configurations for exit popup

--- src/stick-to-me.js
+++ src/stick-to-me.js
@@ -7,3 +7,41 @@
  *
  */
 
+(function ($) {
+
+    $.stickToMe = function ( configs ) {
+
+        var defaults = {
+            layer: "",
+            fadespeed: 400,
+            trigger: ['top'],
+            maxtime : 0,
+            mintime : 0,
+            delay: 0,
+            interval: 0,
+            maxamount : 0,
+            cookie : false,
+            bgclickclose : true,
+            escclose : true,
+            onleave : function (e) {},
+            disableleftscroll : true	// chrome disable
+        }
+
+
+        var configuration = $.extend({}, defaults, configs);
+
+        $(configuration.layer).hide();
+
+        var startuptime = new Date().getTime();
+        var windowHeight = $(window).height();
+        var windowWidth = $(window).width();
+        var offsetbind = false;
+        var howmanytimes = 0;
+        var lasttime = 0;
+        var chromealert = true;
+        var lastx, lasty = 0;
+
+    }
+
+});
+
