787a58377f35df0a712807b0cc096b4f81099d40
Guilherme Assemany <guilhermeassemany@hotmail.com>
2017-03-18 22:25:50 -0300

    Finished exit popup script

--- src/stick-to-me.js
+++ src/stick-to-me.js
@@ -195,21 +195,44 @@
                 if (settings.bgclickclose)
                 {
                     $('.stick_block_layer').click(function(){
-                        gl_cl();
+                        stick_close();
                     });
                 }
                 if (settings.escclose)
                 {
                     $("body").keyup(function(e){
                         if(e.which == 27){
-                            gl_cl();
+                            stick_close();
                         }
                     });
                 }
             }
         }
 
+    };
+
+    function getamount(cname) {
+        var name = cname + "=";
+        var ca = document.cookie.split(';');
+        for(var i=0; i<ca.length; i++) {
+            var c = ca[i];
+            while (c.charAt(0)==' ') c = c.substring(1);
+            if (c.indexOf(name) != -1) return c.substring(name.length,c.length);
+        }
+        return 0;
     }
 
-});
+    function stick_close()
+    {
+        $('.stick_container').fadeOut(function() { $(this).remove(); });
+        $('.stick_block_layer').fadeOut(function() { $(this).remove(); });
+        $.removeData( document.body, "stick_var" );
+    }
+
+    $.stick_close = function()
+    {
+        stick_close();
+    }
+
+}( jQuery ));
 
